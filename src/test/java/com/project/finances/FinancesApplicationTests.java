ef9cfdec4edabc66a32ec6660a8d93c98cb1c991
joao flavio <flaviosenne123@gmail.com>
2022-02-22 09:31:05 -0300

    chore: add dependences and structure package

---
+++ src/test/java/com/project/finances/FinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.finances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
