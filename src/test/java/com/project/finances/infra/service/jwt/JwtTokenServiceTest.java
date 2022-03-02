f93796b6a13fb984e3d9ee7e751c5434bcc48864
joao flavio <flaviosenne123@gmail.com>
2022-03-02 17:18:32 -0300

    test: test json web token class 100% coverage

---
+++ src/test/java/com/project/finances/infra/service/jwt/JwtTokenServiceTest.java
@@ -0,0 +1,96 @@
+package com.project.finances.infra.service.jwt;
+
+import org.assertj.core.api.BDDAssertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+
+@ExtendWith(SpringExtension.class)
+@TestPropertySource(properties = {"secretKey = secret", "expireIn : 10000"})
+class JwtTokenServiceTest {
+
+    @Mock
+    private JwtTokenService jwtTokenService;
+
+    private final MockHttpServletRequest request = new MockHttpServletRequest();
+
+    @BeforeEach
+    void setup(){
+        jwtTokenService = new JwtTokenService();
+
+        ReflectionTestUtils.setField(jwtTokenService, "secretKey", "it's a security key");
+        ReflectionTestUtils.setField(jwtTokenService, "expireIn", "10000");
+    }
+
+
+    @Test
+    @DisplayName("Should return a token when payload is provider")
+    void  generateToken(){
+        String payload = "id";
+        String result = jwtTokenService.generateToken(payload);
+
+        BDDAssertions.assertThat(result).isNotNull().isNotEqualTo(payload);
+    }
+
+    @Test
+    @DisplayName("Should return payload when token is provider")
+    void  decodeToken(){
+        String payload = "id";
+        String token = jwtTokenService.generateToken(payload);
+
+        String result = jwtTokenService.decodeToken(token);
+
+        BDDAssertions.assertThat(result).isNotNull().isEqualTo(payload);
+    }
+
+    @Test
+    @DisplayName("Should return token of request")
+    void  resolveToken(){
+        request.addHeader("Authorization", "Bearer token");
+
+        String result = jwtTokenService.resolveToken(request);
+
+        BDDAssertions.assertThat(result).isNotNull().isEqualTo("token");
+    }
+
+    @Test
+    @DisplayName("Should return null when invalid token in request")
+    void  resolveInvalidToken(){
+        request.addHeader("Authorization", "invalid-token");
+
+        String result = jwtTokenService.resolveToken(request);
+
+        BDDAssertions.assertThat(result).isNull();
+    }
+
+    @Test
+    @DisplayName("Should return true when token valid")
+    void  validToken(){
+        String token = jwtTokenService.generateToken("payload");
+
+        boolean result = jwtTokenService.tokenIsValid(token);
+
+        BDDAssertions.assertThat(result).isNotNull().isTrue();
+    }
+
+    @Test
+    @DisplayName("Should return false when token invalid")
+    void  invalidToken(){
+        String token = jwtTokenService.generateToken("payload");
+
+        ReflectionTestUtils.setField(jwtTokenService, "secretKey", "other key");
+        boolean result = jwtTokenService.tokenIsValid(token);
+
+        BDDAssertions.assertThat(result).isNotNull().isFalse();
+    }
+
+
+
+}
